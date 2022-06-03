c4855178ec68549e95fac696565de23c5126ccde
olatubora-felix <olatubora1996@gmail.com>
2022-06-03 11:46:36 +0100

    Pricing app first push

---
+++ data.js
@@ -0,0 +1,21 @@
+export const prices = [
+  {
+    title: "Basic",
+    storage: "100GB",
+    price: "1.99/Month",
+    classes: "bg-slate-700 text-white rounded-xl hover:bg-violet-700 duration-200"
+  },
+   {
+    title: "standard",
+    storage: "200GB",
+     price: "3.99/Month",
+    classes: "bg-violet-700 text-white rounded-xl hover:bg-violet-700 duration-200"
+  },
+    {
+    title: "primium",
+    storage: "2TB",
+    price: "8.99/Month",
+    classes: "bg-slate-700 text-white rounded-xl hover:bg-violet-700 duration-200"
+ },
+
+]
---
+++ src/App.jsx
@@ -0,0 +1,95 @@
+
+import './App.css'
+import { prices } from '../data';
+import { Fragment } from 'react';
+
+function App() {
+  return (
+    // Global Container
+    <div className="flex items-center justify-center min-h-screen bg-slate-800">
+      <div className="flex flex-col my-6 space-y-6 md:space-y-0 md:space-x-6 md:flex-row md:my-0">
+        {
+          prices.map(price => (
+          <Fragment key={price.title}>
+              {/* First Col */}
+        <div  className={price.classes}>
+          {/* Upper container */}
+          <div className="p-8 mx-3 mt-3 rounded-l-xl bg-slate-800">
+            <div className="text-center uppercase">{price.title}</div>
+            <h2 className="text-5xl mt-10 font-serif text-center">{price.storage}</h2>
+            <h3 className="mt-2 text-center">{price.price}</h3>
+            <div className="flex justify-center">
+              <a href="#" className='inline-block px-10 py-3 my-6 text-center border border-violet-600 hover:bg-violet-800 hover:border-violet-800 rounded-lg  duration-200'>Purpase</a>
+            </div>
+          </div>
+          {/* Border */}
+          <div className="border-t border-slate-700"></div>
+          {/* Lower container */}
+          <div className="p-8 mx-3 mb-3 rounded-b-xl bg-slate-800">
+            {/*List Contain */}
+            <div className="flex flex-col space-y-2">
+              {/* List Item 1*/}
+              <div className="flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-5 h-5"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+                <div className="text-sm ml-1">1{price.storage} of storage</div>
+              </div>
+
+               {/* List Item 2*/}
+              <div className="flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-5 h-5"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+                <div className="text-sm ml-1">Option to add members</div>
+              </div>
+
+               {/* List Item 3*/}
+              <div className="flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-5 h-5"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+                <div className="text-sm ml-1">Extra member benefits</div>
+              </div>
+            </div>
+          </div>
+              </div>
+          </Fragment>
+          ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App
